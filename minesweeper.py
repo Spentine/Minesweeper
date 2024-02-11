17c18bb05059cb3a0eb0c560a9a09df7f4a390a5
Spentine <114318049+Spentine@users.noreply.github.com>
2024-02-11 10:12:54 -0800

    Update minesweeper.py
    make it playable

--- minesweeper.py
+++ minesweeper.py
@@ -6,8 +6,14 @@ class Minesweeper:
     self.height = settings["height"]
     self.mines = settings["mines"]
     self.field = []
+    self.marks = []
+    self.playing = True
+    self.revealed = 0
+
     for i in range(self.height):
       self.field.append([0] * self.width)
+      self.marks.append([0] * self.width)
+
     remainingTiles = self.width * self.height
     remainingMines = self.mines
     for row in self.field:
@@ -17,15 +23,58 @@ class Minesweeper:
           remainingMines -= 1
         remainingTiles -= 1
 
+  def returnNeighbors(self, row, column):
+    checkR = [row - 1, row, row + 1]
+    checkC = [column - 1, column, column + 1]
+    if row == self.height - 1:
+      del checkR[2]
+    if row == 0:
+      del checkR[0]
+    if column == self.width - 1:
+      del checkC[2]
+    if column == 0:
+      del checkC[0]
+    return checkR, checkC
+
+  def calculateNeighbors(self, row, column):
+    checkR, checkC = self.returnNeighbors(row, column)
+
+    neighbors = 0
+
+    for i in checkR:
+      for j in checkC:
+        neighbors += int(self.field[i][j] == -1)
+
+    return neighbors
+
+  def reveal(self, row, column):
+    row = int(row)
+    column = int(column)
+    if self.field[row][column] == -1:
+      self.playing = False
+      return False
+    self.field[row][column] = 1
+
+    if self.calculateNeighbors(row, column) == 0:
+      checkR, checkC = self.returnNeighbors(row, column)
+      for i in checkR:
+        for j in checkC:
+          if self.field[i][j] == 0:
+            self.reveal(i, j)
+
   def consoleField(self):
     assignment = {
       0: ":;",
-      -1: "##",
+      -1: "[]",
     }
     r = ""
-    for i in self.field:
-      for j in i:
-        r += assignment[j]
+    for i in range(self.height):
+      for j in range(self.width):
+        k = self.field[i][j]
+        if k == 1:
+          r += str(self.calculateNeighbors(i, j)) + "_"
+        else:
+          r += assignment[k]
       r += "\n"
     return r
 
@@ -35,4 +84,6 @@ settings = {
   "mines": 10,
 }
 x = Minesweeper(settings)
-print(x.consoleField())
+while x.playing:
+  print(x.consoleField())
+  x.reveal(input("row "), input("column "))
